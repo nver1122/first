babc6fc32624b063f16d0d5e28d213ea338a98c6
nver1122 <36419800+nver1122@users.noreply.github.com>
2018-02-25 19:46:18 +0400

    Add files via upload

--- s.js
+++ s.js
@@ -1,32 +1,25 @@
-var clickv = -1;
-function s1() {
-	
-	console.log(clickv);
-	clickv++;
 
-	 var s = 1;
-	var obj = document.getElementById('lab1');
-	function get() {
-		 if (s<10000)  {
-		
-		setTimeout(get,50);
-		var x = s*2;
-		var q = s/10;
-		document.getElementById('li1').innerHTML = s;
-		document.getElementById('li2').innerHTML = x;
-		document.getElementById('li3').innerHTML = q;
-		leftl = qar.offsetLeft;
-		qar.style.transform = 'rotate'+'('+s*4+'deg'+')';
-		qar.style.left = leftl +s/10+'px';
-		qar.style.transition = 10 + 'ms';
-		s++;
-		if (s<10) {
-			qar.style.top =  '10'+ s +'px';
-		}
-		
-}
-		
-		
+document.getElementById('nav').onmouseover = function (event) {
+	var target = event.target;
+	if (target.className == 'menu-item') {
+		var s = target.getElementsByClassName('submenu');
+		closeMenu();
+		s[0].style.display='block';
 	}
-	setTimeout(get,50);
+}
+document.onmouseover = function(event){
+	var target = event.target;
+	console.log(event.target);
+	if (target.className!='menu-item' && target.className!='submenu') {
+		closeMenu();
+	}
+}
+
+
+function closeMenu() {
+var menu = document.getElementById('nav');
+var subm = document.getElementsByClassName('submenu');
+for (var i = 0; i<subm.length; i++) {
+	subm[i].style.display = 'none';
+}
 }
